a9d501a87f00364304ec66987c167e18c95bbbf1
Naman21nv <namannv28@gmail.com>
2026-03-16 17:45:35 +0530

    model object is used instead of session of HttpSession for sending data from HomeControlle to  return.jsp

--- src/main/WebApp/return.jsp
+++ src/main/WebApp/return.jsp
@@ -3,8 +3,7 @@
 <html>
 
 <body>
-<h1>Result is:  <%out.println(session.getAttribute("result"));%> </h1>
-<h1>Result is:  <%=session.getAttribute("result")%> </h1>
+
 <h1>Result is:  ${result} </h1> <!-- this is called jstl (java server pages tag library) way -->
 
 <!-- all works and give same result -->
--- src/main/java/org/example/springboot_web/HomeController.java
+++ src/main/java/org/example/springboot_web/HomeController.java
@@ -3,6 +3,7 @@ package org.example.springboot_web;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,20 +16,20 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(int num1, int num2 , HttpSession session){
+    public String add(int num1, int num2 , Model model){
         //public String add(@RequestParam("num1")int num, @RequestParam("num2")int num209 , HttpSession session){
         /**
          * public String add(@RequestParam("num1")int num, @RequestParam("num2")int num209 , HttpSession session)
          *
          * when we want to use any other name than the name going in the url of the browser we can do like this
          * we can assign this variable is holding the value of this url parameter using @RequestParam
+         *
+         * for travel from jsp to controller we have use model here
          */
 
         int c =num1+ num2;
-       
 
-       
-       session.setAttribute("result", c); 
+       model.addAttribute("result" , c);
 
        return "return.jsp";
     }
